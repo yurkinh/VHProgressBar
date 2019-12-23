6756b1d70a43810f02a8567b28c23256fb98dde3
Yuriy Holembyovskyy <yuriy.holembyovskyy@authoritypartners.com>
2019-12-23 13:57:40 +0200

    Initial commit

---
+++ VHProgressBar/HorizontalProgressBar.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.ComponentModel;
+using CoreGraphics;
+using Foundation;
+using UIKit;
+
+namespace VHProgressBar
+{
+    [DesignTimeVisible(true)]
+    public class HorizontalProgressBar : UIView
+    {
+        UIView progressView;
+        UIViewPropertyAnimator animator;
+        bool isAnimating;
+
+        public HorizontalProgressBar() 
+        {
+            InitProgressView();
+        }
+
+        public HorizontalProgressBar(NSCoder coder) : base(coder)
+        {
+            InitProgressView();
+        }
+
+        UIColor bgColor = UIColor.White;
+        [Export(nameof(BGColor)), Browsable(true)]
+        public UIColor BGColor
+        {
+            get => bgColor;
+            set
+            {
+                bgColor = value;
+                ConfigureView();
+            }
+        }
+
+        UIColor barColor = UIColor.FromRGBA(red: 52 / 255, green: 181 / 255, blue: 240 / 255, alpha: 1);
+        [Export(nameof(BarColor)), Browsable(true)]
+        public UIColor BarColor
+        {
+            get => barColor;
+            set
+            {
+                barColor = value;
+                ConfigureView();
+            }
+        }
+
+        UIColor frameColor = UIColor.FromRGBA(red: 161 / 255, green: 161 / 255, blue: 161 / 255, alpha: 1);
+        [Export(nameof(FrameColor)), Browsable(true)]
+        public UIColor FrameColor
+        {
+            get => frameColor;
+            set
+            {
+                frameColor = value;
+                ConfigureView();
+            }
+        }
+
+        nfloat frameBold = 0.1f;
+        [Export(nameof(FrameBold)), Browsable(true)]
+        public nfloat FrameBold
+        {
+            get => frameBold;
+            set
+            {
+                frameBold = value;
+                ConfigureView();
+            }
+        }
+
+        nfloat pgHeight = 20f;
+        [Export(nameof(PGHeight)), Browsable(true)]
+        public nfloat PGHeight
+        {
+            get => pgHeight;
+            set
+            {
+                pgHeight = value;
+                ConfigureView();
+            }
+        }
+
+        nfloat pgWidth = 200f;
+        [Export(nameof(PGWidth)), Browsable(true)]
+        public nfloat PGWidth
+        {
+            get => pgWidth;
+            set
+            {
+                pgWidth = value;
+                ConfigureView();
+            }
+        }
+
+        private void InitProgressView()
+        {
+            progressView = new UIView();
+            AddSubview(progressView);
+        }
+
+        private void ConfigureProgressView()
+        {
+            progressView.BackgroundColor = BarColor;
+            progressView.Frame = new CGRect(progressView.Frame.Location.X, Bounds.Location.Y , 0, PGHeight);
+            progressView.Layer.CornerRadius = PGHeight / 2;
+        }
+
+        private void ConfigureView()
+        {
+            BackgroundColor = BGColor;
+            Layer.BorderWidth = FrameBold;
+            Layer.BorderColor = FrameColor.CGColor;
+            Frame = new CGRect(Frame.Location, new CGSize(PGWidth, PGHeight));
+            Layer.CornerRadius = PGHeight / 2;
+        }
+
+        public void AnimateProgress(nfloat duration, nfloat progressValue)
+        {
+            if (0 > progressValue || progressValue > 1.0)
+            {
+                return;
+            }
+            ConfigureProgressView();
+            UICubicTimingParameters timing = new UICubicTimingParameters(UIViewAnimationCurve.EaseIn);
+            animator = new UIViewPropertyAnimator(duration, timing);
+            animator.AddAnimations(() => progressView.Frame =
+            new CGRect(progressView.Frame.Location, new CGSize(progressView.Frame.Size.Width +PGWidth*progressValue, progressView.Frame.Size.Height)));
+
+            animator.StartAnimation();
+        }
+
+        public void StartAnimation(string type, nfloat duration)
+        {
+            if (isAnimating)
+                return;
+            switch (type)
+            {
+                case "normal":
+                    RunAnimation(reverse: false, duration: duration);
+                    break;
+                case "reverse":
+                    RunAnimation(reverse: true, duration: duration);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        void RunAnimation(bool reverse, nfloat duration)
+        {
+            ConfigureProgressView();
+            animator = UIViewPropertyAnimator.CreateRunningPropertyAnimator(duration, delay: 0.0, UIViewAnimationOptions.CurveEaseInOut, () =>
+            {
+                SetAnimationRepeatCount(1000);
+                SetAnimationRepeatAutoreverses(reverse);
+                progressView.Frame = new CGRect(progressView.Frame.Location, new CGSize(progressView.Frame.Size.Width+PGWidth, progressView.Frame.Size.Height));
+            }, null);
+            isAnimating = true;
+            animator.StartAnimation();
+        }
+
+        public void StopAnimation()
+        {
+            if (!isAnimating)
+                return;
+
+            isAnimating = false;
+            animator.StopAnimation(true);
+        }
+
+        public nfloat GetProgress() => progressView.Frame.Size.Width;
+    }
+}
---
+++ VHProgressBar/ProgressBarTest/ViewController.cs
@@ -0,0 +1,54 @@
+﻿using CoreGraphics;
+using Foundation;
+using System;
+using UIKit;
+using VHProgressBar;
+
+namespace ProgressBarTest
+{
+    public partial class ViewController : UIViewController
+    {
+        
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            var verticalProgressBar = new VerticalProgressBar();
+            var horizontalProgressBar = new HorizontalProgressBar();
+            View.AddSubview(verticalProgressBar);
+            View.AddSubview(horizontalProgressBar);
+
+            //adjust layout
+            verticalProgressBar.Frame = new CGRect(new CGPoint(100, 100), verticalProgressBar.Frame.Size);
+            verticalProgressBar.FrameBold = 0.5f;
+            verticalProgressBar.BGColor = UIColor.Green;
+            verticalProgressBar.BarColor = UIColor.Red;
+            verticalProgressBar.FrameColor = UIColor.Black;
+
+            horizontalProgressBar.Frame = new CGRect(new CGPoint(100, 500), horizontalProgressBar.Frame.Size);
+            horizontalProgressBar.FrameBold = 0.1f;
+            horizontalProgressBar.BGColor = UIColor.Yellow;
+            horizontalProgressBar.BarColor = UIColor.Blue ;
+            horizontalProgressBar.FrameColor = UIColor.Black;
+
+            // start simple animation
+            verticalProgressBar.AnimateProgress(duration: 2.0f, progressValue: 0.7f);
+            horizontalProgressBar.AnimateProgress(duration: 2.0f, progressValue: 0.5f);
+
+            // start repeat animation
+            // you can choose type "normal" or "reverse"
+            //horizontalProgressBar.StartAnimation(type: "normal", duration: 2.0f);
+            //stop repeat animation  
+            //horizontalProgressBar.StopAnimation();
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
